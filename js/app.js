43c311c1e35c1cdfcc25c0bfab02a6a5c732fdff
stevim <i.m.steveim@gmail.com>
2023-03-31 16:36:40 -0400

    Add necessary assets

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let deck1 = []
+let deck2 = []
+let deck3 = []
+let deck4 = []
+let deck5 = []
+let deck6 = []
+let deck7 = []
+let deck8 = []
+
+
+let deck1El = document.getElementById('deck1')
+let deck2El = document.getElementById('deck2')
+let deck3El = document.getElementById('deck3')
+let deck4El = document.getElementById('deck4')
+let deck5El = document.getElementById('deck5')
+let deck6El = document.getElementById('deck6')
+let deck7El = document.getElementById('deck7')
+let deck8El = document.getElementById('deck8')
