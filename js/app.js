f149a1c1d65fec74133d2a536aa725d8f0e12b3b
stevim <i.m.steveim@gmail.com>
2023-04-01 20:09:26 -0400

    create reset button

--- js/app.js
+++ js/app.js
@@ -24,10 +24,12 @@ document.getElementById(`startBtn`).addEventListener(`click`, function() {
 document.getElementById('nextTurnBtn').addEventListener(`click`, function () {
   handleNextTurn()
 })
+document.getElementById('resetBtn').addEventListener(`click`), function() {
+  handleReset()
+}
 
 function init() {
   startingDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
-
   }
 
 
@@ -53,4 +55,8 @@ console.log(deck5)
 
 function handleNextTurn() {
 
+}
+
+function handleReset() {
+  
 }
