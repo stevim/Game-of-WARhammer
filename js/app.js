b8328455c2fbf326e2cecb4aafe96ba4e8a92c92
stevim <i.m.steveim@gmail.com>
2023-04-02 13:49:43 -0400

    Superior startingDeck Array

--- js/app.js
+++ js/app.js
@@ -36,8 +36,63 @@ document.getElementById('resetBtn').addEventListener(`click`, function() {
 })
 
 function init() {
-  startingDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
-  }
+  // startingDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+
+  startingDeck = [
+    {title: "dA", value: 1},
+    {title: "sA", value: 1},
+    {title: "hA", value: 1},
+    {title: "cA", value: 1},
+    {title: "d02", value: 2},
+    {title: "s02", value: 2},
+    {title: "h02", value: 2},
+    {title: "c02", value: 2},
+    {title: "d03", value: 3},
+    {title: "s03", value: 3},
+    {title: "h03", value: 3},
+    {title: "c03", value: 3},
+    {title: "d04", value: 4},
+    {title: "s04", value: 4},
+    {title: "h04", value: 4},
+    {title: "c04", value: 4},
+    {title: "d05", value: 5},
+    {title: "s05", value: 5},
+    {title: "h05", value: 5},
+    {title: "c05", value: 5},
+    {title: "d06", value: 6},
+    {title: "s06", value: 6},
+    {title: "h06", value: 6},
+    {title: "c06", value: 6},
+    {title: "d07", value: 7},
+    {title: "s07", value: 7},
+    {title: "h07", value: 7},
+    {title: "c07", value: 7},
+    {title: "d08", value: 8},
+    {title: "s08", value: 8},
+    {title: "h08", value: 8},
+    {title: "c08", value: 8},
+    {title: "d09", value: 9},
+    {title: "s09", value: 9},
+    {title: "h09", value: 9},
+    {title: "c09", value: 9},
+    {title: "d10", value: 10},
+    {title: "s10", value: 10},
+    {title: "h10", value: 10},
+    {title: "c10", value: 10},
+    {title: "dJ", value: 11},
+    {title: "sJ", value: 11},
+    {title: "hJ", value: 11},
+    {title: "cJ", value: 11},
+    {title: "dQ", value: 12},
+    {title: "sQ", value: 12},
+    {title: "hQ", value: 12},
+    {title: "cQ", value: 12},
+    {title: "dK", value: 13},
+    {title: "sK", value: 13},
+    {title: "hK", value: 13},
+    {title: "cK", value: 13}, 
+  ]
+}
 
 init()
 
